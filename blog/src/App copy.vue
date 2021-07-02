4f5440602249c84fad8177c599309d69a3e4c6e9
everglow <78077569+okchanho@users.noreply.github.com>
2021-07-02 15:13:31 +0900

    7/2

---
+++ blog/src/App copy.vue
@@ -0,0 +1,97 @@
+<template>
+    <!-- 시작 -->
+    <div>
+        <!-- 상단바 -->
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="/home">Vuelog</a>
+                <button
+                    class="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarNavDropdown"
+                    aria-controls="navbarNavDropdown"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a
+                                class="nav-link active"
+                                aria-current="page"
+                                href="/home"
+                                >Home</a
+                            >
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/detail">Detail</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/list">List</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a
+                                class="nav-link dropdown-toggle"
+                                href="#"
+                                id="navbarDropdownMenuLink"
+                                role="button"
+                                data-bs-toggle="dropdown"
+                                aria-expanded="false"
+                            >
+                                Dropdown link
+                            </a>
+                            <ul
+                                class="dropdown-menu"
+                                aria-labelledby="navbarDropdownMenuLink"
+                            >
+                                <li>
+                                    <a class="dropdown-item" href="#">Action</a>
+                                </li>
+                                <li>
+                                    <a class="dropdown-item" href="#"
+                                        >Another action</a
+                                    >
+                                </li>
+                                <li>
+                                    <a class="dropdown-item" href="#"
+                                        >Something else here</a
+                                    >
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <router-view :blogdata="blogdata"></router-view>
+        
+        <!-- <a href="/list">리스트</a>
+        <br />
+        <router-link to="/list">리스트페이지</router-link>
+        <router-link to="/home">홈페이지</router-link> -->
+
+
+        
+    </div>
+</template>
+
+<script>
+import blogdata from "./assets/blog.js";
+
+export default {
+    name: "App",
+
+    data() {
+        return {
+            blogdata: blogdata,
+        };
+    },
+    components: {},
+};
+</script>
+
+<style></style>
