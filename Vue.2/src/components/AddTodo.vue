d484ce56265616f93a5c68cf60d123048c72a9f0
Arthur <chanho.ok@cubewiz.co.kr>
2022-04-20 15:17:22 +0900

    Vue 2.0
    vue 2.0에대한 기초적인것들

---
+++ Vue.2/src/components/AddTodo.vue
@@ -0,0 +1,31 @@
+<template>
+    <input 
+        v-model="todoText"
+        type="text" 
+        class="w-100 p-2" 
+        placeholder="Type todo"
+        @keyup.enter="addTodo"
+    >
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            todoText: '',
+        }
+    },
+    methods: {
+        addTodo(e) {
+            
+            this.$store.dispatch('addTodo',  e.target.value);
+            this.todoText= '';
+            
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ Vue.2/src/components/UserList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div> 
+      <div v-for="user in users" :key="user.id">
+          {{ user.name }}
+      </div>
+  </div>
+</template>
+
+<script>
+import { mapState, mapActions } from 'vuex'
+export default {    
+    created(){
+        this.getusers();        
+    },
+    computed:{
+        ...mapState(['users'])
+    },
+    methods:{
+        ...mapActions([ 'getUsers'])
+        // getusers(){
+        //     this.$store.dispatch('getUsers')
+        // }
+    }
+}
+</script>
+
+<style>
+
+</style>
